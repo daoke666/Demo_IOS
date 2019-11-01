c3c098ac92a81b547021f43b446f2b3f63411dac
dengying <dengying@imilab.com>
2019-11-01 17:41:18 +0800

    更改主视图控制器

--- Demo/Demo/SceneDelegate.m
+++ Demo/Demo/SceneDelegate.m
@@ -1,4 +1,5 @@
 #import "SceneDelegate.h"
+#import "ViewController.h"
 
 @interface SceneDelegate ()
 
@@ -11,6 +12,34 @@
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    
+    UIWindowScene *windowScene = (UIWindowScene *)scene;
+    
+    self.window = [[UIWindow alloc] initWithWindowScene:windowScene];
+    self.window.frame = windowScene.coordinateSpace.bounds;
+    
+    [self.window setBackgroundColor:[UIColor whiteColor]];
+    
+    self.window.rootViewController = [ViewController new];
+    
+    [self.window makeKeyAndVisible];
+    
+}
+
+//老的Xcode版本在 Appdelegate类中didFinishLaunchingWithOptions方法中添加
+-(void)initRootViewController{
+    
+    //更改根视图控制器
+    //self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    //[self.window setBackgroundColor:[UIColor whiteColor]];
+    
+    //ViewController * myView = [[ViewController alloc]init];
+    //MyViewController * myView = [[MyViewController alloc]init];
+    
+    //UINavigationController * nav = [[UINavigationController alloc]initWithRootViewController:myView];
+    //self.window.rootViewController = nav;
+    
+    //[self.window makeKeyAndVisible];
 }
 
 
