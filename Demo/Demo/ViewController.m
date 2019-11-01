74bf27f7186a1af08ec6b0d83bd4e5403f8b251f
dengying <dengying@imilab.com>
2019-11-01 16:34:00 +0800

    add Button Lable

--- Demo/Demo/ViewController.m
+++ Demo/Demo/ViewController.m
@@ -17,7 +17,81 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self initButtonLable];
 }
 
+
+-(void)initButtonLable{
+    //lable
+    int w = self.view.frame.size.width;
+    int h = self.view.frame.size.height;
+    
+    UILabel* lable = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 50)];
+    
+    lable.center = CGPointMake(w/2, h/2);
+    lable.textAlignment = UITextAlignmentCenter;
+    lable.backgroundColor = [UIColor clearColor];
+    lable.text = @"代码布局 Hello World! ViewController";
+    [self.view addSubview:lable];
+    
+    //button
+    UIButton* button = [[UIButton alloc] initWithFrame:CGRectMake(100, 200, 150, 100)];
+    
+    //按钮背景
+    button.backgroundColor = [UIColor redColor];
+    
+    //按钮文字
+    [button setTitle:@"普通Button" forState:UIControlStateNormal];
+    [button setTitle:@"高亮Button" forState:UIControlStateHighlighted];
+    
+    //Button监听事件，非常重要
+    /**
+     *addTarget:目标（让谁做这个事情）
+     *action:方法（做什么事情-->方法）
+     *forControlEvents:事件
+     */
+    [button addTarget:self action:@selector(onbtnClick:)forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:button];
+}
+
+-(void)onbtnClick:(UIButton*)sender{
+    
+    NSLog(@"click btn");
+    
+
+    //页面跳转
+    //MyViewController *myViewController = [[MyViewController alloc] init];
+    //myViewController.view.backgroundColor = [UIColor blueColor];
+    
+    //导航控制器入栈的方式切换页面
+    //[self.navigationController pushViewController:myViewController animated:YES];
+    
+    //模态切换的方式切换页面
+    //[self presentViewController:myViewController animated:YES completion:nil];
+    
+    //self.modalPresentationStyle = UIModalPresentationFullScreen;
+    //[self presentModalViewController:myViewController animated:YES];
+    
+
+    /*
+     //初始化提示框
+     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提示" message:@"按钮被点击了" preferredStyle:  UIAlertControllerStyleAlert];
+    
+     [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+         
+         NSLog(@"click btn ok");
+     }]];
+    
+     [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+        NSLog(@"click btn cancle");
+     }]];
+    
+     //弹出提示框；
+     [self presentViewController:alert animated:true completion:nil];
+     */
+}
 
 @end
